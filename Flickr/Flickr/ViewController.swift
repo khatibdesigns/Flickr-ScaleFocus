d3ef64be343e271a1703ac60d1d691943188c6ed
Nader Khatib <eng.nader@live.com>
2019-12-10 11:21:43 +0200

    Initial project added

---
+++ Flickr/Flickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Khatib Designs on 12/10/19.
+//  Copyright © 2019 Khatib Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
